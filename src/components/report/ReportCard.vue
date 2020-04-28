8ab992011738afca062802e763754632e26a52d5
hambalee <hambalee@outlook.com>
2020-04-28 12:37:25 +0700

    report/summary card

---
+++ src/components/report/ReportCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="report_card">
+    <v-card v-ripple class="mx-auto" max-width="344" outlined>
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title class="headline mb-1">{{ title }}</v-list-item-title>
+          <v-list-item-subtitle class="title">{{subtitle | currency}}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReportCard',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    subtitle: {
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
